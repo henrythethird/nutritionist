244313a0c9bbb79df2096ad37414667223b9ec15
Patrick Kaufmann <patrick.kaufmann@pwc-digital.ch>
2017-02-12 18:45:10 +0100

    Added files to localhost instead of CDN. Recipe template added (redirect if no ingredient)

--- src/AppBundle/Controller/IngredientController.php
+++ src/AppBundle/Controller/IngredientController.php
@@ -4,21 +4,39 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Ingredient\Ingredient;
+use AppBundle\Entity\Ingredient\Recipe;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class IngredientController extends BaseDavController
 {
     /**
      * @Route("/ingredient/show/{id}", name="ingredient_show")
-     * @Template(":ingredient:show.html.twig")
+     * @Template("ingredient/show.html.twig")
      */
     public function showAction(Ingredient $ingredient)
     {
+        if ($ingredient instanceof Recipe) {
+            return $this->redirectToRoute("recipe_show", [
+                'id' => $ingredient->getId()
+            ]);
+        }
+
         return [
             'dav' => $this->getDav(),
             'ingredient' => $ingredient
+        ];
+    }
+
+    /**
+     * @Route("/recipe/show/{id}", name="recipe_show")
+     * @Template("ingredient/recipe.html.twig")
+     */
+    public function recipeAction(Recipe $recipe)
+    {
+        return [
+            'dav' => $this->getDav(),
+            'recipe' => $recipe
         ];
     }
 }
