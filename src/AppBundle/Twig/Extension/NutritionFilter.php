b994346ad9099c35ec05197e5d32eeb3c7510d73
Patrick Kaufmann <patrick.kaufmann@pwc-digital.ch>
2017-02-22 23:18:26 +0100

    Made nutrition info great again

--- src/AppBundle/Twig/Extension/NutritionFilter.php
+++ src/AppBundle/Twig/Extension/NutritionFilter.php
@@ -4,8 +4,26 @@ namespace AppBundle\Twig\Extension;
 
 class NutritionFilter extends \Twig_Extension
 {
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('nutrition', [$this, 'nutritionFilter']),
+        );
+    }
+
+    /**
+     * @param int|float $number
+     * @return string
+     */
+    public function nutritionFilter($number, $length = 2)
+    {
+        $price = number_format($number, $length);
+
+        return $price;
+    }
+
     public function getName()
     {
-        return 'nutritionFormat';
+        return 'app_nutrition_filter';
     }
 }
