39071cdf02699bbc86f0f6f7d3c3c591faacbb08
Patrick Kaufmann <patrick.kaufmann@pwc-digital.ch>
2017-02-23 06:54:46 +0100

    Set up API

---
+++ src/AppBundle/Controller/ApiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Ingredient\Recipe;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ApiController extends BaseDavController
+{
+    /**
+     * @Route("/api/recipetable/{id}", name="api_recipe_table")
+     * @Template("nutrition/info.html.twig")
+     */
+    public function recipeTableAction(Recipe $recipe)
+    {
+        return [
+            'nutrition' => $recipe->getNutrition(),
+            'dav' => $this->getDav()
+        ];
+    }
+}
